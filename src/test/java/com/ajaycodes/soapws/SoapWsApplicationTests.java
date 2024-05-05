580037dfa2fcb542b5100e3276e0d9d8f317b224
Ajay <ajay_kumar_1990@live.com>
2024-05-05 18:02:46 +0530

    Initial commit

---
+++ src/test/java/com/ajaycodes/soapws/SoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajaycodes.soapws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
